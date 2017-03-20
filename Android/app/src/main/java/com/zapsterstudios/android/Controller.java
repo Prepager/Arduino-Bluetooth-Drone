6502588c420f7698f92dcd3ddf64b9dd1c288d4f
ZapsterStudios <andreas@zapsterstudios.com>
2017-03-20 22:04:02 +0100

    Android project

---
+++ Android/app/src/main/java/com/zapsterstudios/android/Controller.java
@@ -0,0 +1,13 @@
+package com.zapsterstudios.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Controller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controller);
+    }
+}
