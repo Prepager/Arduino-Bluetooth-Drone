7491c51080eee4a07e0c7b004a0ae61cd88f6280
ZapsterStudios <andreas@zapsterstudios.com>
2017-03-21 00:09:16 +0100

    Android bootstrap

--- Android/app/src/main/java/com/zapsterstudios/android/Controller.java
+++ Android/app/src/main/java/com/zapsterstudios/android/Controller.java
@@ -1,13 +1,20 @@
 package com.zapsterstudios.android;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Controller extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Create view
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
+
+        // Force landscape rotation
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
